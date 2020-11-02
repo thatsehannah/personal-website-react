9843e9f28621daeab5d7ecfd743fbc126ea9dd64
E. Hannah <echannah631@gmail.com>
2020-11-01 17:06:21 -0800

    changed fadeinduration on muse cards

--- src/components/Muses/musesData.js
+++ src/components/Muses/musesData.js
@@ -27,21 +27,21 @@ export const muses = [
     pic: { url: fam, title: "Elliot's parents" },
     description:
       "Making my family proud is the biggest motivator in life. They've been my biggest supporters and have always been there for me no matter what. Every move I make, I make sure to seek their advice and counsel. They've molded me to who I am today and they are the best parents I could ever ask for. Even though they're on the other side of the country, I think of them every single day. I am so grateful for them and love them so much.",
-    fadeInDuration: 700,
+    fadeInDuration: 600,
   },
   {
     title: "I ♥️ L.A.",
     pic: { url: overlook, title: "Overlook of Los Angeles" },
     description:
       'Besides the nice weather, the Lakers, landscape, etc., the main thing I love about L.A. is that it helped me culminate a new definition of "success". I have developed a different outlook on it since moving here and I am more determined to be successful in my career. Even though it is a bit expensive to live in L.A., it is definitely a place I can see myself my settling and raising a family.',
-    fadeInDuration: 900,
+    fadeInDuration: 700,
   },
   {
     title: "Nipsey Hussle",
     pic: { url: nipsey, title: "Nipsey Hussle" },
     description:
       'I became a fan of Nipsey Hussle when my fiancée and I moved to L.A in Fall 2018. His music helped ease my anxiety of moving here in the fashion we did and also helped me become more comfortable to call this place "home". Nipsey helped me see L.A. in a different light from what I saw on television. He also inspired me to stay dedicated to reach my goals and to keep striving because my hard work WILL pay off in the long run. Thank you Nipsey, The Marathon Continues 🕊.',
-    fadeInDuration: 1100,
+    fadeInDuration: 800,
   },
   {
     title: "Music",
@@ -53,20 +53,20 @@ export const muses = [
       tooltip: "My Soundcloud page",
       link: "https://soundcloud.com/thatsehannah/tracks",
     },
-    fadeInDuration: 1300,
+    fadeInDuration: 900,
   },
   {
     title: "Basketball",
     pic: { url: kobemjbron, title: "Kobe, MJ, Bron" },
     description:
       "Basketball was my first love; I fell in love with it in 4th grade, and I've been a fan ever since. I enjoy watching it as well as playing it. I'm pretty good at it I would think but that's another conversation. Basketball taught me discipline and gave me confidence to strive to be the best at any and everything I do. Well since you asked, my top 5 favorite players of all-time are LeBron James, Michael Jordan, Allen Iverson, Shaq, and the late-great Kobe Bryant. Rest in peace Kobe & Gianna. Mamba Forever.",
-    fadeInDuration: 1500,
+    fadeInDuration: 1000,
   },
   {
     title: "Technology",
     pic: { url: tech, title: "Workstation with code on computer" },
     description:
       "One of the biggest reasons why I wanted to become a developer is because of the ever-evolving capabilities of technology. Technology is without a doubt one of the most significant entities in our modern lives. I've always been attracted to how the IoT, such as computers and mobile phones, work behind the scenes. That solely drove me to learn how to code and want to continue to be hands-on with the growing world of tech.",
-    fadeInDuration: 1700,
+    fadeInDuration: 1100,
   },
 ];
